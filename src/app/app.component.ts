cd1ab2e35ae3cb7dea4465eb8dcbc1679d31ee4e
jcgomez <gb.jc@outlook.com>
2020-04-15 15:35:46 -0600

    Juego del ahorcado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,55 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Ahorcado';
+
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+
+  intentos = 0;
+  ganar = false;
+  perder = false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor () {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+
+  comprobar(letra) {
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+
+  verificaGane() {
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if(palabraEvaluar === this.palabra) {
+      this.ganar = true;
+    }
+
+    if( this.intentos >= 9) {
+      this.perder = true;
+    }
+  }
+
+  existeLetra(letra) {
+    if(this.palabra.indexOf(letra) < 0) {
+      this.intentos ++;
+      return false;
+    }
+  }
 }
